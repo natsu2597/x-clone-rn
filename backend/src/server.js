61047d660993f3000757a762efae59968e88e301
natsu2597 <atulrishi1997@gmail.com>
2025-10-20 19:35:46 +0530

    notification route created and rate limiting and bot detection implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,10 +3,11 @@ import { ENV } from './config/env.js';
 import { connectDB } from './config/db.js';
 import cors from 'cors';
 import { clerkMiddleware } from '@clerk/express';
+import { arcjetMiddleware } from "./middlewares/arcjet.middleware.js";
 import  userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
-
+import notificationRoutes from "./routes/notification.route.js";
 
 const app = express();
 
@@ -16,10 +17,12 @@ app.use(express.json());
 
 
 app.use(clerkMiddleware());
+app.use(arcjetMiddleware);
 
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 app.use((err,req,res, next) => {
     console.error("Unhandled error: ",err);
