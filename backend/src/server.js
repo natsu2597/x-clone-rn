9cee65a66811515fa889ad1cd27c0eb31bd92e8d
natsu2597 <atulrishi1997@gmail.com>
2025-10-20 23:17:04 +0530

    small change in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,6 +19,8 @@ app.use(express.json());
 app.use(clerkMiddleware());
 app.use(arcjetMiddleware);
 
+app.get("/", (req, res) => res.send("Hello from server"));
+
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
