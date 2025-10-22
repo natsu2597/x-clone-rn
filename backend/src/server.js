7dd6d1b7d07c5479e9cd09f8120e4d5c336c2755
natsu2597 <atulrishi1997@gmail.com>
2025-10-22 18:03:31 +0530

    messages page frontend implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,7 @@ import  userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import notificationRoutes from "./routes/notification.route.js";
+import searchRoutes from "./routes/search.route.js";
 
 const app = express();
 
@@ -24,6 +25,7 @@ app.get("/", (req, res) => res.send("Hello from server"));
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
+app.use("/api/search", searchRoutes);
 app.use("/api/notifications", notificationRoutes);
 
 app.use((err,req,res, next) => {
--- mobile/app/(tabs)/search.tsx
+++ mobile/app/(tabs)/search.tsx
@@ -17,7 +17,7 @@ const SearchScreen = () => {
   return (
     <SafeAreaView className='flex-1 bg-white'>
       <View className='px-4 py-3 border-b border-gray-100'>
-        <View className='flex-row items-center bg-gray-100 rounded-full px-4'>
+        <View className='flex-row items-center bg-gray-100 rounded-full px-4 py-2'>
           <Feather name='search' size={20} color="#657786" />
           <TextInput 
           placeholder='Search X'
